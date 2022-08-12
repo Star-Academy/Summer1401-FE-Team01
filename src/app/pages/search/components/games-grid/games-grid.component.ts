0245ca4b0e8c873b33fc9d16a4ee7aba45347bbf
Diba <dibaaamir80@gmail.com>
2022-08-12 16:04:18 +0430

    make game page and game grid better

--- src/app/pages/search/components/games-grid/games-grid.component.ts
+++ src/app/pages/search/components/games-grid/games-grid.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Game} from '../../../../models/game.model';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-games-grid',
@@ -9,9 +10,18 @@ import {Game} from '../../../../models/game.model';
 export class GamesGridComponent {
     @Input() public games: Array<Game> = [];
 
+    public constructor(private router: Router) {}
+
     public gameSalePercentage(game: Game): number | null {
         if (game.price === game.priceOnSale) return null;
 
         return 100 - Math.floor((game.priceOnSale / game.price) * 100);
+    }
+
+    public async openGamePage(game: Game): Promise<void> {
+        await this.router.navigate(['/game'], {
+            state: {game: game},
+            queryParams: {id: game.id},
+        });
     }
 }
