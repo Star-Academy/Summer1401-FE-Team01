69e946953e61348bf58567e7568e72ff9059e1c6
Diba <dibaaamir80@gmail.com>
2022-08-13 12:13:37 +0430

    add profile pic alter

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,6 +2,9 @@ import {Component} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
 import {User} from '../../models/user.model';
+import {SnackbarService} from '../../services/snackbar.service';
+import {SnackbarTheme} from '../../enums/snackbar-theme.enum';
+import {UserService} from '../../services/user.service';
 
 @Component({
     selector: 'app-profile',
@@ -10,12 +13,35 @@ import {User} from '../../models/user.model';
 })
 export class ProfileComponent {
     public user!: User | null;
-    public constructor(private router: Router, private authService: AuthService) {
+    public constructor(
+        private router: Router,
+        private authService: AuthService,
+        private snackbarService: SnackbarService,
+        private userService: UserService
+    ) {
         this.user = authService.cachedUser;
+    }
+
+    public get profilePhotoSrc(): string {
+        return this.user?.avatar || '../../../assets/images/default-profile-picture.svg';
     }
 
     public async logout(): Promise<void> {
         await this.authService.logout();
         await this.router.navigateByUrl('/');
+    }
+
+    public submitPhoto(event: Event): void {
+        const file = (event.target as HTMLInputElement)!.files!.item(0)!;
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = async (): Promise<void> => {
+            const base64Img = reader.result as string;
+            const response = await this.userService.alterUserInfo('avatar', base64Img);
+            if (response) this.user!.avatar = base64Img;
+        };
+        reader.onerror = (_): void => {
+            this.snackbarService.show({text: 'دریافت تصویر با خطا روبرو شد!', theme: SnackbarTheme.DANGER});
+        };
     }
 }
