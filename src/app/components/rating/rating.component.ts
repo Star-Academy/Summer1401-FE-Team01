b1fe166db505c6cce2ff7203636979a3673ef979
Diba <dibaaamir80@gmail.com>
2022-08-14 19:30:53 +0430

    delete trash from rating.component

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -10,52 +10,17 @@ export class RatingComponent {
     @Input() public darkMode: boolean = false;
     @Input() public value: number = 0;
 
-    @Output() public valueChange = new EventEmitter<number>();
-
-    public isHovering: boolean = false;
-    public hoverValue: number = 0;
-
     public readonly MAX_VALUE = 5;
 
-    @ViewChild('rating') public rating!: ElementRef<HTMLDivElement>;
-
-    public constructor(private roundNumberToHalfPipe: RoundNumberToHalfPipe) {}
-
     public get fullStars(): Array<number> {
-        return new Array(Math.floor(this.isHovering ? this.hoverValue : this.value));
+        return new Array(Math.floor(this.value));
     }
 
     public get hasHalfStar(): boolean {
-        return (this.isHovering ? this.hoverValue : this.value).toString().includes('.');
+        return this.value.toString().includes('.');
     }
 
     public get emptyStars(): Array<number> {
-        return new Array(Math.floor(this.MAX_VALUE - (this.isHovering ? this.hoverValue : this.value)));
-    }
-
-    public onHoverStart(event: MouseEvent): void {
-        this.isHovering = true;
-        this.hoverValue = this.calculateHoverValue(event);
-    }
-
-    public onHovering(event: MouseEvent): void {
-        this.hoverValue = this.calculateHoverValue(event);
-    }
-
-    private calculateHoverValue(event: MouseEvent): number {
-        const rect = this.rating.nativeElement.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-
-        return this.roundNumberToHalfPipe.transform((x / rect.width) * this.MAX_VALUE);
-    }
-
-    public onHoverEnd(): void {
-        this.isHovering = false;
-        this.hoverValue = 0;
-    }
-
-    public onClick(event: MouseEvent): void {
-        this.value = this.calculateHoverValue(event);
-        this.valueChange.emit(this.value);
+        return new Array(Math.floor(this.MAX_VALUE - this.value));
     }
 }
