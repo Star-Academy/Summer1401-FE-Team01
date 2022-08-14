4f49198616bb7f7321b94d5e50b23d2abbff75cd
Diba <dibaaamir80@gmail.com>
2022-08-14 11:53:54 +0430

    small fixes

--- src/app/pages/game-page/pipes/include.pipe.spec.ts
+++ src/app/pages/game-page/pipes/include.pipe.spec.ts
@@ -1,7 +1,7 @@
 import {IncludePipe} from './include.pipe';
 
 describe('IncludePipe', () => {
-    it('create an instance', () => {
+    it('should an instance', () => {
         const pipe = new IncludePipe();
         expect(pipe).toBeTruthy();
     });
