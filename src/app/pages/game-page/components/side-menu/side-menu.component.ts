4366ade3fc6e568819da8e49a6949e77fb139fb8
Diba <dibaaamir80@gmail.com>
2022-08-14 22:25:36 +0430

    add bookmark and favourite to game page

--- src/app/pages/game-page/components/side-menu/side-menu.component.ts
+++ src/app/pages/game-page/components/side-menu/side-menu.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Game, ImageType} from '../../../../models/game.model';
+import {UserBookmarkFavouriteService} from '../../../../services/user-bookmark-favourite.service';
 
 @Component({
     selector: 'app-side-menu',
@@ -11,11 +12,37 @@ export class SideMenuComponent {
 
     public ImageType = ImageType;
 
+    public constructor(private userBookmarkFavouriteService: UserBookmarkFavouriteService) {}
+
     public get developerCompanies(): string {
         return this.game.developerCompanies.map((c) => c.name).join(', ');
     }
 
     public get publisherCompanies(): string {
         return this.game.publisherCompanies.map((c) => c.name).join(', ');
+    }
+
+    public get isInBookmarks(): boolean {
+        return (this.userBookmarkFavouriteService.cachedBookmarkIds || []).includes(this.game.id);
+    }
+
+    public get isInFavourites(): boolean {
+        return (this.userBookmarkFavouriteService.cachedFaveIds || []).includes(this.game.id);
+    }
+
+    public async removeFromBookmark(): Promise<void> {
+        await this.userBookmarkFavouriteService.removeFromBookmarks(this.game.id);
+    }
+
+    public async addToBookmarks(): Promise<void> {
+        await this.userBookmarkFavouriteService.addToBookmarks(this.game.id);
+    }
+
+    public async removeFromFavourites(): Promise<void> {
+        await this.userBookmarkFavouriteService.removeFromFaves(this.game.id);
+    }
+
+    public async addToFavourites(): Promise<void> {
+        await this.userBookmarkFavouriteService.addToFaves(this.game.id);
     }
 }
